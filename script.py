d9677c3bb437e46a2d99e8e4c165cb4404fe8c35
Alfredomg7 <alfredomg4000@gmail.com>
2023-09-13 12:27:05 -0700

    calculations to identify the highest return and lowest risk portfolios added, legend moved to the lower right position, also removed the weights.mean calculation since it is irrelevant and added the weights for each portfolio in the portfolios dataframe

--- script.py
+++ script.py
@@ -32,11 +32,15 @@ correlation_matrix = daily_returns.corr()
 
 # Find optimal portfolios varying weights using randomization
 num_portfolios = 10000
-results = np.zeros((4, num_portfolios))
+results = np.zeros((3, num_portfolios))
+portfolio_weights = []
 for i in range(num_portfolios): 
   weights = np.random.random(len(stocks_symbols))
   weights /= np.sum(weights)  
+
+  # Calculate portfolio annualized return
   portfolio_return = np.sum(weights * annualized_returns)
+
   # Calculate portfolio risk using correlation matrix and weights
   portfolio_risk = np.dot(weights.T, np.dot(correlation_matrix, weights))
   portfolio_risk = np.sqrt(portfolio_risk)
@@ -44,26 +48,43 @@ for i in range(num_portfolios):
   results[0, i] = portfolio_return
   results[1, i] = portfolio_risk
   results[2, i] = portfolio_return / portfolio_risk
-  results[3, i] = weights.mean()
-
+  portfolio_weights.append(weights)
+  
 # Create a DataFrame of the calculated portfolios
-portfolios = pd.DataFrame(
-    results.T,
-    columns=["Return", "Risk", "Sharpe Ratio", "Weighted Mean"]
-)
+columns = ["Return", "Risk", "Sharpe Ratio"]
+portfolios = pd.DataFrame(results.T, columns=columns)
+weights_df = pd.DataFrame(portfolio_weights, columns=stocks_symbols)
+portfolios = pd.concat([portfolios, weights_df], axis=1)
 
-# Fin the portfolio with the highest Sharpe ratio
+
+# Find the portfolio with the highest Sharpe ratio
 max_sharpe_portfolio =  portfolios.iloc[portfolios["Sharpe Ratio"].idxmax()]
+
+# Find the portfolio with the highest return
+max_return_portfolio = portfolios.iloc[portfolios["Return"].idxmax()]
+
+# Find the portfolio with the lowest risk
+min_risk_portfolio = portfolios.iloc[portfolios["Risk"].idxmin()]
 
 # Plot efficient frontier by visualizing the portfolios' risk and return
 plt.figure(figsize=(10, 6))
 plt.scatter(portfolios["Risk"], portfolios["Return"], c=portfolios["Sharpe Ratio"], cmap="viridis", marker="o", s=10, alpha=0.3)
 plt.scatter(max_sharpe_portfolio["Risk"], max_sharpe_portfolio["Return"], color="red", marker="*", s=100, label="Max Sharpe Ratio Portfolio")
+plt.scatter(max_return_portfolio["Risk"], max_return_portfolio["Return"], color="green", marker="^", s=100, label="Max Return Portfolio")
+plt.scatter(min_risk_portfolio["Risk"], min_risk_portfolio["Return"], color="blue", marker="s", s=100, label="Min Risk Portfolio")
 plt.xlabel("Risk")
 plt.ylabel("Annualized Return")
 plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter(1.0))  
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 plt.title("Efficient Frontier with Sharpe Ratios")
 plt.colorbar(label="Sharpe Ratio")
-plt.legend()
+plt.legend(loc="lower right")
 plt.show()
+
+# Print information for each portfolio
+print("Portfolio with Highest Sharpe Ratio:")
+print(max_sharpe_portfolio)
+print("\nPortfolio with Highest Return:")
+print(max_return_portfolio)
+print("\nPortfolio with Lowest Risk:")
+print(min_risk_portfolio)
