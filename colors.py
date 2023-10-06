0850562069419e75b0b8d923c73828daecf190cc
Alfredomg7 <alfredomg4000@gmail.com>
2023-10-06 12:52:40 -0700

    module created to save a dictionary with a color pallete for charts

---
+++ colors.py
@@ -0,0 +1,13 @@
+sector_colors = {
+    "Energy": "#1f77b4", 
+    "Materials": "#ff7f0e", 
+    "Industrials": "#8c564b", 
+    "Utilities": "#d62728", 
+    "Health Care": "#9467bd", 
+    "Financials": "#2ca02c", 
+    "Consumer Discretionary": "#e377c2", 
+    "Consumer Staples": "#7f7f7f", 
+    "Technology": "#bcbd22", 
+    "Communication Services": "#17becf", 
+    "Real Estate": "#9edae5"
+}
